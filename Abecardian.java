926b4115a871a11bc656e125d6c00e503d9dedd3
Pashweetie <Pashweetie@gmail.com>
2013-10-07 08:55:33 -0600

    Finished hw2

--- Abecardian.java
+++ Abecardian.java
@@ -2,6 +2,7 @@ public class Abecardian {
 	private static String alph = "abcdefghijklmnopqrstuvwxyz";
 	public static void main(String[] args) {
 		System.out.println(isAbecedarian("you"));
+		System.out.println(isDoubloon("lolo"));
 	}
 	public static boolean isAbecedarian(String s) {
 		boolean answer = false;
@@ -22,5 +23,24 @@ public class Abecardian {
 			return true;
 		}
 		return false; 
+	}
+	public static boolean isDoubloon(String s){
+		boolean shortAnswer = false;
+		boolean answer = false;
+		boolean finalAnswer = false;
+		for (int i = 0; i<s.length(); i++) {
+			answer = false;
+			for (int j = 1; j < s.length()-1; j++) {
+				char numba1 = s.charAt(i);
+				char numba2 = s.charAt(j);
+				if (numba1==numba2) {
+					answer = true;
+				}
+			}
+			if(!answer){
+				finalAnswer = false;
+			}
+		}
+		return answer;
 	}
 }
